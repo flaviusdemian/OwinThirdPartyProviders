db3544de4a425ef85dfa027a4dc83166c403c5c9
Flavius-Radu Demian <flaviusdemian91@hotmail.com>
2014-10-16 00:07:12 +0300

    almost done

---
+++ OwinOAuthProvidersDemo/Models/CreateRepoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinOAuthProvidersDemo.Models
+{
+    public class CreateRepoModel
+    {
+        public CreateRepoModel()
+        {
+            Collaboratos = new Dictionary<string, string>();
+        }
+        public String TrackName { get; set; }
+        public String TeamName { get; set; }
+        public String MentorName { get; set; }
+
+        public Dictionary<String, String> Collaboratos { get; set; }
+    }
+}
---
+++ OwinOAuthProvidersDemo/Models/GithubCreateRepositoryResponseRepoDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace OwinOAuthProvidersDemo.Models
+{
+    public class GithubCreateRepositoryResponseRepoDetails
+    {
+        [JsonProperty(PropertyName = "full_name")]
+        public String FullName { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public String Id { get; set; }
+
+        [JsonProperty(PropertyName = "html_url")]
+        public String RepoUrl { get; set; }
+
+    }
+}

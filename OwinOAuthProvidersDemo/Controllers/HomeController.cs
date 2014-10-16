81f896b8e0aec32c50be51f9c91603ecc1bf6478
Flavius-Radu Demian <flaviusdemian91@hotmail.com>
2014-10-16 19:47:36 +0300

    added new solution

---
+++ OwinOAuthProvidersDemo/Controllers/HomeController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Owin.Security.Providers;
+using OwinOAuthProvidersDemo.Models;
+
+namespace OwinOAuthProvidersDemo.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        String token = "734253fe7f1742e11060992a8597ab95435adf56";
+        private HttpClient client = null;
+        public async virtual Task<ActionResult> Index()
+        {
+            var lines = FileReaderBusinessLogic.ReadFiles(Server.MapPath("/Input/Input.txt"));
+            if (lines != null && lines.Length > 1)
+            {
+                string currentLine = null;
+                List<CreateRepoModel> reposToCreate = new List<CreateRepoModel>();
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    try
+                    {
+                        currentLine = lines[i];
+                        if (i == 0)
+                        {
+                            continue;
+                        }
+                        string[] partsOfCurrentLines = currentLine.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                        if (partsOfCurrentLines != null && partsOfCurrentLines.Length > 0)
+                        {
+                            string currentUserGithubUsername = null;
+                            string currentUserEmail = null;
+                            CreateRepoModel currentRepoToCreate = new CreateRepoModel();
+                            // track name  
+                            currentRepoToCreate.TrackName = partsOfCurrentLines[1].Trim();
+                            //team name
+                            currentRepoToCreate.TeamName = partsOfCurrentLines[2].Trim();
+                            //mentor name
+                            currentRepoToCreate.MentorName = partsOfCurrentLines[3].Trim();
+
+                            List<string> collaboratorsEntry = partsOfCurrentLines.ToList();
+                            for (int l = 0; l < 4; l++)
+                            {
+                                collaboratorsEntry.RemoveAt(0);
+                            }
+
+                            for (int j = 0; j < collaboratorsEntry.ToArray().Length; j++)
+                            {
+                                if (j % 3 == 0)
+                                {
+                                    currentUserGithubUsername = collaboratorsEntry.ElementAt(j);
+                                    try
+                                    {
+                                        currentUserEmail = collaboratorsEntry.ElementAt(j + 1);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        ex.ToString();
+                                    }
+                                    currentRepoToCreate.Collaboratos.Add(currentUserGithubUsername, currentUserEmail);
+                                }
+                            }
+                            await CreateRepoAndAddCollaborators(currentRepoToCreate);
+                            reposToCreate.Add(currentRepoToCreate);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                }
+            }
+            return View();
+        }
+        private async Task CreateRepoAndAddCollaborators(CreateRepoModel currentRepoToCreate)
+        {
+            try
+            {
+                if (currentRepoToCreate != null && currentRepoToCreate.Collaboratos != null &&
+                    String.IsNullOrWhiteSpace(currentRepoToCreate.TrackName) == false
+                    && String.IsNullOrWhiteSpace(currentRepoToCreate.TeamName) == false
+                    && String.IsNullOrWhiteSpace(currentRepoToCreate.MentorName) == false)
+                {
+                    client = new HttpClient();
+                    GithubCreateRepositoryRequest model = new GithubCreateRepositoryRequest()
+                    {
+                        Name = String.Format("{0}-{1}", currentRepoToCreate.TrackName, currentRepoToCreate.TeamName),
+                        Description = String.Format("Mentor-{0}", currentRepoToCreate.MentorName),
+                        Homepage = "https://github.com",
+                        Private = false,
+                        HasIssues = true,
+                        HasWiki = true,
+                        HasDownloads = true
+                    };
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "https://api.github.com/meta");
+
+                    try
+                    {
+                        var result = await client.PostAsync(new Uri(String.Format("https://api.github.com/user/repos?access_token={0}", token)),
+                                     new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json"));
+                        result.EnsureSuccessStatusCode();
+                        string content = await result.Content.ReadAsStringAsync();
+                        var parsedObject = JsonConvert.DeserializeObject<GithubCreateRepositoryResponseRepoDetails>(content);
+
+                        foreach (var collaboratorsEntry in currentRepoToCreate.Collaboratos)
+                        {
+                            try
+                            {
+                                result = await client.PutAsync(new Uri(
+                                 String.Format("https://api.github.com/repos/{0}/collaborators/{1}?access_token={2}", parsedObject.FullName, collaboratorsEntry.Key.Trim(), token)), null);
+                                result.EnsureSuccessStatusCode();
+                            }
+                            catch (Exception ex)
+                            {
+                                ex.ToString();
+                            }
+                        }
+                        //https://api.github.com/repos/slown1/GRWXUZEMIJ/collaborators/vladisac
+                        //string owner = "slown1";
+                        //string projectId = "GRWXUZEMIJ";
+                        //string usertoInvite = "denisdenes";
+
+
+                        //result = await client.Get()
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        public virtual ActionResult SigninGithub()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public virtual ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public virtual ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OwinOAuthProvidersDemo/RandomStringGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace OwinOAuthProvidersDemo
+{
+    public class RandomStringGenerator
+    {
+        public static string RandomString(int size)
+        {
+            var builder = new StringBuilder();
+            var random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26*random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
